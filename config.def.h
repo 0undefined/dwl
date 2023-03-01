40c4301f7d1e4128f996521a5833a95a31c43b25
0scar <qgt268@alumni.ku.dk>
2023-03-01 20:00:23 +0100

    Update customization: remove sloppy focus
    The focus-on-mouse-movement bothered me to much

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 #define TERMINAL "foot"
 
 /* appearance */
-static const int sloppyfocus               = 1;  /* focus follows mouse */
+static const int sloppyfocus               = 0;  /* focus follows mouse */
 static const int bypass_surface_visibility = 0;  /* 1 means idle inhibitors will disable idle tracking even if it's surface isn't visible  */
 static const int smartborders              = 1;
 static const unsigned int borderpx         = 2;  /* border pixel of windows */
